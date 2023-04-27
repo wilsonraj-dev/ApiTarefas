e10ad3dd362127960e389a3f3a246fa40ccdc4dd
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-04-27 20:06:41 -0300

    Criação dos métos de PUT e DELETE

--- ApiTarefas/ApiTarefas/Program.cs
+++ ApiTarefas/ApiTarefas/Program.cs
@@ -21,7 +21,7 @@ app.MapGet("/tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync()
 app.MapGet("/tarefas/{id}", async (int id, AppDbContext db) =>
     await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
 
-app.MapGet("/tarefas/concluida", async (AppDbContext db) => 
+app.MapGet("/tarefas/concluida", async (AppDbContext db) =>
                                  await db.Tarefas.Where(x => x.Concluida).ToListAsync());
 
 app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
@@ -30,6 +30,32 @@ app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
     await db.SaveChangesAsync();
 
     return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+});
+
+app.MapPut("/tarefas/{id}", async (int id, Tarefa tarefa, AppDbContext db) =>
+{
+    var tf = await db.Tarefas.FindAsync(id);
+
+    if (tf is null)
+        return Results.NotFound();
+
+    tf.Nome = tarefa.Nome;
+    tf.Concluida = tarefa.Concluida;
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/tarefas/{id}", async (int id, AppDbContext db) =>
+{
+    if (await db.Tarefas.FindAsync(id) is Tarefa tarefa)
+    {
+        db.Tarefas.Remove(tarefa);
+        await db.SaveChangesAsync();
+        Results.Ok(tarefa);
+    }
+
+    Results.NotFound();
 });
 
 app.Run();
